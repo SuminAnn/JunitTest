384510e19608b92e79f94af93348929bce111310
SuminAnn <annsumin2066@naver.com>
2023-02-16 11:55:14 +0900

    repo TEST pk 초기화 안되는 부분 수정

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 
 @DataJpaTest //DB와 관련된 컴포넌트들만 메모리에 로드
@@ -64,6 +65,7 @@ public class BookRepositoryTest {
     }// 트랜잭션 종료(저장된 데이터 초기화) -> 각각 테스트는 분리를 시키는게 좋다
 
     // 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
     @Test
     public void select_test(){
         //given
@@ -81,6 +83,7 @@ public class BookRepositoryTest {
     // 책 수정
 
     // 책 삭제
+    @Sql("classpath:db/tableInit.sql")
     @Test
     public void delect_test(){
         //given
@@ -105,4 +108,9 @@ public class BookRepositoryTest {
  -> 테스트 실행(테스트 환경)
  통합 테스트는 전부를 다 테스트
  단위테스트 (특정 부분만 메모리에 로드)
+
+* 테스트는 순서가 보장이 안된다 -> 순서를 보장하고 싶은경우 @Order(번호)를 활용
+* 테스트 메서드가 하나 실행 후 종료되면 데이터가 초기화된다.(@Transactional로 인해)
+ - PK(auto_increment) 값이 초기화가 안된다
+   -> 해결방법 : @Sql 활용 (id를 활용하는 test에서는 테이블을 drop후에 다시 생성해주는것이 좋다)
 */
