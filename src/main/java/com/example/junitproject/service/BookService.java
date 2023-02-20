d911bdc35b18f4230010cdb0a45004e5bf631023
SuminAnn <annsumin2066@naver.com>
2023-02-20 17:12:07 +0900

    책 등록 Service

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -1,8 +1,33 @@
 package com.example.junitproject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
+import com.example.junitproject.web.dto.BookRespDto;
+import com.example.junitproject.web.dto.BookSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
-    
+
+    private final BookRepository bookRepository; //final은 객체 생성 시점에 값이 들어와야한다(@RequiredArgsConstructor은 final 필드에 주입__DI)
+
+    // 1. 책등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto save(BookSaveReqDto dto){
+       Book bookPS = bookRepository.save(dto.toEntity());
+       return new BookRespDto().toDto(bookPS);
+    }
+
+    //2. 책 목록보기
+
+    //3. 책한건보기
+
+    //4. 책삭제
+
+    //5. 책수정
 }
--- src/main/java/com/example/junitproject/web/dto/BookRespDto.java
+++ src/main/java/com/example/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,22 @@
 package com.example.junitproject.web.dto;
 
+import com.example.junitproject.domain.Book;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class BookRespDto {
+    private Long id;
+    private String titile;
+    private String author;
+
+    public BookRespDto toDto(Book bookPS){
+        this.id = bookPS.getId();
+        this.titile = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
     
 }
