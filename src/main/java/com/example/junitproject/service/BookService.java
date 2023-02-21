a3e216dd0a05de338c0ac28d21d9cff7854c0826
SuminAnn <annsumin2066@naver.com>
2023-02-21 16:41:46 +0900

    책 목록보기 완료 stream 이해

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -1,5 +1,8 @@
 package com.example.junitproject.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,11 @@ public class BookService {
     }
 
     //2. 책 목록보기
+    public List<BookRespDto> select(){
+        return bookRepository.findAll().stream()
+        .map(new BookRespDto()::toDto)
+        .collect(Collectors.toList()); // bookRepository.findAll()을 return하는 경우 Book타입으로 return이 되기때문에 dto로 변경해서 return을 해줘야한다
+    }
 
     //3. 책한건보기
 
